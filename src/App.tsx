bbb49adc5a87b0458d809e32c47c7ad8218ca3c2
johnpaolosantos <johnpaolosantos527@gmail.com>
2026-06-12 09:33:01 +0800

    habit tracker

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Header } from "./components/Header";
+import { HabitForm } from "./components/HabitForm";
+import { HabitList } from "./components/HabitList";
+import { HabitProvider } from "./context/HabitProvider";
+import { useState } from "react";
+import { addWeeks, eachDayOfInterval, endOfWeek, startOfWeek } from "date-fns";
+
+export default function App() {
+    const [weekOffSet, setWeekOffset] = useState(0);
+    const week = addWeeks(new Date(), weekOffSet);
+    const visibleDates = eachDayOfInterval({
+        start: startOfWeek(week, { weekStartsOn: 1 }),
+        end: endOfWeek(week, { weekStartsOn: 1 }),
+    });
+
+    return (
+        <HabitProvider>
+            <div className="max-w-2xl mx-auto p-4 flex flex-col gap-4">
+                <Header 
+                    visibleDates={visibleDates} 
+                    onNext={() => setWeekOffset(o => o+1)}
+                    onPrev={() => setWeekOffset(o => o-1)}
+                />
+                <HabitForm />
+                <HabitList visibleDates={visibleDates}/>
+            </div>
+        </HabitProvider>
+    );
+}
---
+++ src/context/HabitProvider.tsx
@@ -0,0 +1,35 @@
+import { isSameDay } from "date-fns";
+import { type ReactNode } from "react";
+import { HabitContext, type Habit } from "./useHabit";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+type HabitProviderProps = {
+    children: ReactNode
+}
+
+export function HabitProvider({ children }: HabitProviderProps) {
+    const [habits, setHabits] = useLocalStorage<Habit[]>("Habits", []);
+
+    function addHabit(name: string) {
+        setHabits(curr => [...curr, { id: crypto.randomUUID(), name, completions: [new Date()] }]);
+    }
+
+    function deleteHabit(id: string) {
+        setHabits(curr => curr.filter(h => h.id !== id));
+    }
+
+    function toggleHabit(id: string, date: Date) {
+        setHabits(curr => (
+            curr.map(h => {
+                if (h.id !== id) return h
+                const alreadyDone = h.completions.some(c => isSameDay(c, date));
+                const completions = alreadyDone ? h.completions.filter(c => !isSameDay(c, date)) : [...h.completions, date]
+
+                return { ...h, completions }
+            })
+        ));
+    }
+    return <HabitContext value={{ habits, addHabit, toggleHabit, deleteHabit }}> {children} </HabitContext>
+}
+
+
